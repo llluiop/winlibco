fd2371c4e2fad4c1417c3acaaf3e73f06d147ccb
llluiop <290522165@163.com>
2017-11-22 15:43:29 +0800

    add comments

--- libco/libco/coctx.cpp
+++ libco/libco/coctx.cpp
@@ -30,7 +30,7 @@ __declspec(naked) void coctx_swap(Coctx * cur, Coctx * pending)
 		mov esp, [esp+4]
 		lea esp, [esp+32]
 
-		push eax //esp ->parm a 
+		push eax //把cur.reg[7]作为堆栈起始进行操作，压栈等于对reg进行赋值
 
 		push ebp
 		push esi
@@ -41,7 +41,7 @@ __declspec(naked) void coctx_swap(Coctx * cur, Coctx * pending)
 		push [eax-4]
 
 
-		mov esp, [eax+4]
+		mov esp, [eax+4] //把pending.reg[0]作为堆栈进行操作，出栈等于把reg中的值赋给寄存器
 
 		pop eax
 		pop ebx
